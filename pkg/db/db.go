92f924a7a72be4e1ee25dc802f9d0909851fed6d
xs <xies.1@jifenn.com>
2022-03-11 10:41:42 +0800

    feat:

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -31,21 +31,13 @@ type DbOptions struct {
 	// sources 主库 replicas 从库 稍微大一点的 一主多从即可，当前仅提供多从
 	// slaves['从库名称，自定义名称']从库配置
 	// db.Clauses(dbresolver.Use("secondary")).First(&user) 指定secondary 库查询
-	Slaves        map[string]string
-	Level         logger.LogLevel `yaml:"level"`
-	SlowThreshold time.Duration   `yaml:"slowTime"` // 慢查询阀值
+	Slaves                 map[string]string
+	Level                  logger.LogLevel `yaml:"level"`
+	SlowThreshold          time.Duration   `yaml:"slowTime"`               // 慢查询阀值
+	SkipDefaultTransaction bool            `yaml:"skipDefaultTransaction"` // true 开启禁用事物，大约 30%+ 性能提升
 }
 
 var ProviderSet = wire.NewSet(New, NewDb)
-
-//type db struct {
-//	gdb *gorm.DB
-//}
-//type ORM interface {
-//	DB() *gorm.DB
-//}
-//
-//var _ ORM = (*db)(nil)
 
 func New(v *viper.Viper) (*DbOptions, error) {
 	var err error
@@ -58,6 +50,7 @@ func New(v *viper.Viper) (*DbOptions, error) {
 	o.MaxOpenConn = 5
 	// 默认配置 8小时，常规使用 1小时
 	o.ConnMaxLifetime = 3600
+	o.SkipDefaultTransaction = true
 	if err = v.UnmarshalKey("db", o); err != nil {
 		return nil, errors.Wrap(err, "unmarshal db option error")
 	}
@@ -70,8 +63,8 @@ func NewDb(c *DbOptions, log *zap.Logger) (*gorm.DB, func(), error) {
 		SlowThreshold: c.SlowThreshold,
 	}
 	lg := NewGL(&lOpts, log)
-	dbconfig := gorm.Config{
-		//SkipDefaultTransaction: true,
+	config := gorm.Config{
+		SkipDefaultTransaction: c.SkipDefaultTransaction,
 		NamingStrategy: schema.NamingStrategy{
 			TablePrefix:   c.PreFix, // 表名前缀，`User` 的表名应该是 `t_users`
 			SingularTable: true,     // 使用单数表名，启用该选项，此时，`User` 的表名应该是 `t_user`
@@ -89,7 +82,7 @@ func NewDb(c *DbOptions, log *zap.Logger) (*gorm.DB, func(), error) {
 		c.Port,
 		c.Database,
 	)
-	gdb, err := gorm.Open(mysql.Open(dsn), &dbconfig)
+	gdb, err := gorm.Open(mysql.Open(dsn), &config)
 	if err != nil {
 		panic(fmt.Sprintf("init db err:%v", err))
 	}
@@ -104,11 +97,4 @@ func NewDb(c *DbOptions, log *zap.Logger) (*gorm.DB, func(), error) {
 		sqlDB.Close()
 	}
 	return gdb, cleanup, nil
-	//return &db{
-	//	gdb: gdb,
-	//}, cleanup, nil
 }
-
-//func (this db) DB() *gorm.DB {
-//	return this.gdb
-//}
